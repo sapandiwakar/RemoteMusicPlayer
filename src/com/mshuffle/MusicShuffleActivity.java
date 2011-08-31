d87cfca0c5793de2939d4b4a82e41c82a90a28af
Sapan Diwakar <diwakar.sapan@gmail.com>
2011-08-31 14:45:16 +0530

    Initial Commit

---
+++ src/com/mshuffle/MusicShuffleActivity.java
@@ -0,0 +1,13 @@
+package com.mshuffle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MusicShuffleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
