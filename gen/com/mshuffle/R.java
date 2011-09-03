ca3b60f974d07bc969f6e4d6f4207a6845f967f2
Sapan Diwakar <diwakar.sapan@gmail.com>
2011-09-03 15:07:16 +0530

    Added the shuffle function

--- gen/com/mshuffle/R.java
+++ gen/com/mshuffle/R.java
@@ -18,16 +18,22 @@ public final class R {
         public static final int test=0x7f020004;
     }
     public static final class id {
-        public static final int activitywebview=0x7f050001;
+        public static final int activitywebview=0x7f050003;
+        public static final int shufflebutton=0x7f050002;
+        public static final int thumbnail=0x7f050001;
         public static final int webview=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int moods=0x7f030001;
-        public static final int recentactivity=0x7f030002;
+        public static final int music=0x7f030002;
+        public static final int recentactivity=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int host=0x7f040002;
+        public static final int json=0x7f040004;
+        public static final int shuffle=0x7f040003;
     }
 }
